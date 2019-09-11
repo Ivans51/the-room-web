210f6c164b77e90a438064c95ada172aae735d47
Ivans <ivans1041@gmail.com>
2019-09-10 22:37:38 -0400

    feature: developer
    Signed-off-by: Ivans <ivans1041@gmail.com>

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 // create smartbutton
-jQuery('nav').before('<div id="smartbutton"></div>');
+jQuery('nav').before('<div id="smartbutton" onclick="menuResponsive()"></div>');
 const smartButton = jQuery('#smartbutton');
 smartButton.append('<div class="buttonline"></div>');
 smartButton.append('<div class="buttonline"></div>');
@@ -8,8 +8,3 @@ smartButton.append('<div class="buttonline"></div>');
 function menuResponsive() {
   jQuery('nav').animate({height: 'toggle'}, 200);
 }
-
-// add click listener
-smartButton.click(function (event) {
-  menuResponsive()
-});
