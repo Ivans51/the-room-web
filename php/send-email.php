b04a817b84bfb562c04921eab4c98d047e4946e4
Ivans <ivans1041@gmail.com>
2019-09-19 03:36:34 -0400

    build: production

--- php/send-email.php
+++ php/send-email.php
@@ -1,23 +1,28 @@
 <?php
 
-$email = isset( $_POST['email'] ) ? $_POST['email'] : '';
-$message  = isset( $_POST['message'] ) ? $_POST['message'] : '';
+$name    = isset($_POST['name']) ? $_POST['name'] : '';
+$email   = isset($_POST['email']) ? $_POST['email'] : '';
+$subject = isset($_POST['subject']) ? $_POST['subject'] : '';
+$message = isset($_POST['message']) ? $_POST['message'] : '';
+$to    = 'ivans1041@gmail.com';
+
+$message = 'Nombre ' . $name . '<br>Email ' . $email . '<br>' . $message;
 
 $logError = '';
 
-$headers = array( 'From: ' . $from );
-if ( ! mail( $from, 'Mensaje de ' . $from, $message, $headers ) ) {
-    $logError = 'Error enviando correo';
+$headers = array('From: ' . $email);
+if (!mail($to, $subject, $message, implode("\r\n", $headers))) {
+  $logError = 'Error enviando correo';
 }
 
-if ( $logError === '' ) {
-	wp_send_json( array(
-		              'message' => 'Correo enviado',
-		              'type'    => 'success'
-	              ) );
+if ($logError === '') {
+  json_encode(array(
+                 'message' => 'Correo enviado',
+                 'type'    => 'success'
+               ));
 } else {
-	wp_send_json( array(
-		              'message' => $logError,
-		              'type'    => 'error'
-	              ) );
+  json_encode(array(
+                 'message' => $logError,
+                 'type'    => 'error'
+               ));
 }
